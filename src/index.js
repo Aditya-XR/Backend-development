bd8d2fa01487aa0f42d833c43a17398c479cd5a3
Your New Name <your-new-email@example.com>
2025-10-12 13:11:59 +0530

    API Error handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-// require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./DB/index.js";
 
  dotenv.config({ path: './env' });
 
 
-connectDB();
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!!!", err);
+})
 
 
 
