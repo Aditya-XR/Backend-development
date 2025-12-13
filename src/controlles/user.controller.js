bb00d5d1e826e629d381006b367486cc3e6535bd
Aditya-XR <adityaccds@gmail.com>
2025-12-13 14:44:00 +0530

    Add getCurrentUser and updateAccountDetails functionalities; implement user data retrieval and account update logic

--- src/controlles/user.controller.js
+++ src/controlles/user.controller.js
@@ -248,17 +248,47 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
     }
 
     user.password = newPassword;
-    await user.save({validateBeforeSave: true});
+    await user.save({validateBeforeSave: false});
 
     return res
     .status(200)
     .json(new ApiResponse(200, null, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const {fullName, email} = req.body;
+
+    if(!fullName || !email){
+        throw new ApiError(400, "Full name and email are required");
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email: email
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAcessToken
+    refreshAcessToken,
+    changeCurrentPassword,
+    getCurrentUser
 };
